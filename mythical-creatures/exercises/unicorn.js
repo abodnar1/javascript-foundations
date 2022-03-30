91c858e087bf025cda6f394cf447d7a835a5ac2c
Amber Bodnar <amber.bodnar@gmail.com>
2022-03-30 19:14:05 -0400

    Pass all unicorn tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,52 @@
+// class Unicorn {
+//   constructor(name, color) {
+//     this.name = name;
+//     if (color === undefined) {
+//       this.color = "white";
+//   } else {
+//     this.color = color;
+//     }
+//   }
+//
+//   isWhite() {
+//     if(this.color != "white") {
+//       return false;
+//     }
+//   }
+//
+//   says(message) {
+//     return `**;* ${message} *;**`;
+//   }
+// };
+
+
+
+//REFACTORED:
+
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || "white";
   }
-}
+
+  isWhite() {
+    if (this.color != "white") {
+      return false;
+    }
+  }
+  says(message) {
+      return `**;* ${message} *;**`;
+  }
+};
 
 module.exports = Unicorn;
+
+
+// class Unicorn {
+//   constructor() {
+//
+//   }
+// }
+
+
+// when you see new if means you're making a new instance
