a1de56e53ebff6d4a16c0e6a9b35fe2ee30062d5
Amber Bodnar <amber.bodnar@gmail.com>
2022-04-02 13:42:49 -0400

    Pass magician test

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,34 @@
+class Magician {
+  constructor({name, assistant, clothing, incantation}) {
+    this.name = "The Great " + name;
+    this.assistant = assistant;
+    this.favoriteAccessory = clothing || "top hat";
+    this.incantation = incantation;
+    this.confidencePercentage = 10;
+  }
+
+  performIncantation(incantation) {
+    return (incantation.toUpperCase()) + "!";
+  }
+
+  performTrick() {
+    this.confidencePercentage += 10;
+    if (this.favoriteAccessory === "top hat") {
+      return `PULL RABBIT FROM TOP HAT`;
+    } else {
+      return `PULL DOVE FROM SLEEVE`;
+    }
+  }
+
+  performShowStopper() {
+    if (this.confidencePercentage >= 100 && this.assistant === true) {
+      return `WOW! The magician totally just sawed that person in half!`;
+    } else {
+      return `Oh no, this trick is not ready!`;
+    }
+  }
+  
+};
+
+
+module.exports = Magician;
