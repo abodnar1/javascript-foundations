f907c82bf77b7fda6e86fa2e6406ba9e24cae458
Amber Bodnar <amber.bodnar@gmail.com>
2022-04-19 09:50:18 -0400

    Refactor pirate test

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -4,30 +4,64 @@ class Pirate {
     this.job = job || "scallywag";
     this.cursed = false;
     this.booty = 0;
-  }
+  };
 
   robShip() {
-    if (this.booty < 500) {
-      this.booty += 100;
-      return `YAARRR!`;
-    } else {
+    if (this.booty === 500) {
       this.cursed = true;
       return `ARG! I\'ve been cursed!`;
+    } else {
+      this.booty += 100;
+      return `YAARRR!`;
     }
-  }
+  };
 
   liftCurse() {
-    if (this.cursed === true) {
+    if (this.cursed) {
       this.booty -= 300;
       this.cursed = false;
-      return `Your curse has been lifted!`;
+      return `Your curse has been lifted!`
     } else {
-      return `You don\'t need to lift a curse!`;
+      return `You don\'t need to lift a curse!`
     }
-  }
+  };
+
 };
 
-
-
-
 module.exports = Pirate;
+
+
+
+// class Pirate {
+//   constructor(name, job) {
+//     this.name = name;
+//     this.job = job || "scallywag";
+//     this.cursed = false;
+//     this.booty = 0;
+//   }
+//
+//   robShip() {
+//     if (this.booty < 500) {
+//       this.booty += 100;
+//       return `YAARRR!`;
+//     } else {
+//       this.cursed = true;
+//       return `ARG! I\'ve been cursed!`;
+//     }
+//   }
+//
+//   liftCurse() {
+//     if (this.cursed === true) {
+//       this.booty -= 300;
+//       this.cursed = false;
+//       return `Your curse has been lifted!`;
+//     } else {
+//       return `You don\'t need to lift a curse!`;
+//     }
+//   }
+// };
+//
+//
+//
+//
+// module.exports = Pirate;
