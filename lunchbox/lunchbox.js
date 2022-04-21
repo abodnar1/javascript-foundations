0f5f9129c2ef83f72666129110e45ad04198efe9
Amber Bodnar <amber.bodnar@gmail.com>
2022-04-21 10:40:48 -0400

    Pass lunchbox test

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,28 @@
 class LunchBox {
+  constructor(lunchbox) {
+    this.owner = lunchbox.owner;
+    this.material = lunchbox.madeOf;
+    this.shape = lunchbox.shape;
+    this.color = lunchbox.color;
+    this.snacks = [];
+  };
 
-}
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  };
+
+  findHealthySnacks() {
+    var healthySnacks = [];
+
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].type.includes("fruit") || this.snacks[i].type.includes("Fruit")) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return healthySnacks;
+  };
+
+};
 
 module.exports = LunchBox;
