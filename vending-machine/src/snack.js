7c5e12663e4dea513f19c50af9f89e7257afa0bc
Amber Bodnar <amber.bodnar@gmail.com>
2022-04-21 11:12:25 -0400

    Pass snack & first half of vending machine test

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,5 +1,23 @@
 class Snack {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.itemsInStock = 0;
+  };
 
-}
+  stockItems(qty) {
+    this.itemsInStock = (this.itemsInStock + qty);
+  };
+
+  removeItem() {
+    if (this.itemsInStock === 0) {
+      return `Sorry, no ${this.name} left in stock!`;
+    } else {
+      this.itemsInStock -- ;
+      return `Item taken! There are now ${this.itemsInStock} items left.`
+    }
+  };
+
+};
 
 module.exports = Snack;
